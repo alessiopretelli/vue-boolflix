a388a3f0b0d71cb82cf6372b5b964d77dfc017bb
Alessio <ale79631@gmail.com>
2021-04-06 17:30:15 +0200

    bandiere chiamate tramite api per verifica

--- main.js
+++ main.js
@@ -16,10 +16,34 @@ var app = new Vue({
                     this.movies = [];
                     this.movies.push(result.data.results);
 
-                    
+                    this.movies[0].forEach((element) => {                        
+                        axios
+                        .get(`https://flagcdn.com/${element.original_language}/codes.json`)
+                        .then((res) => {
+                            //res da' stato 200 a 'en' ma non restituisce alcun png, pur inserendo le bandiere di altri paesi anglofoni; l'ho quindi escluso.
+                            if((res.status == 200) && (element.original_language != 'en')) {
+                                element.original_language = [element.original_language];
+                                element.original_language.push(
+                                    'https://flagcdn.com/16x12/' + element.original_language[0] + '.png',
+                                    'ok'
+                                );
+                            }
+
+                            if(element.original_language == 'en') {
+                                element.original_language = [element.original_language];
+                                element.original_language.push(
+                                    'https://www.countryflags.io/gb/flat/16.png',
+                                    'ok'
+                                );
+                            }
+
+                        });
+                    });
 
                     console.log(this.movies);
-                });     
+                });
+                
+                this.search = '';
             }
         }    
     }
