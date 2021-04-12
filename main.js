ac3ce01e5edb483fb61ae4a1351a393c4a31a9de
Alessio <ale79631@gmail.com>
2021-04-12 10:21:58 +0200

    semafori non funzionante

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ var app = new Vue({
         genreselected: [],
         glis: [],
         typeselected: [],
-        search: ''
+        search: '',
+        verify: [false, false, false]
     },
     mounted() {
         axios
@@ -25,6 +26,7 @@ var app = new Vue({
     methods: {
         searching: function() {
             this.movies = [];
+            this.verify = [false, false, false];
             if (this.search == 0) {
                 this.movies = [];
             } else {
@@ -39,53 +41,60 @@ var app = new Vue({
                         this.movies.push(element);
                     });
 
+                    this.verify[0] = true;
                 });
                 //chiamata API per serie TV con adattamento per interpretazione nell'HTML
-                axios
-                .get('https://api.themoviedb.org/3/search/tv?api_key=1054834a21f5e84aed95192bd4b277cd&', { params: { query: this.search } })
-                .then((re) => {
-                    //metodo adattamento serie TV
-                    this.convert(re);
-                    console.log(this.movies);
-                    //metodo bandiere. A volte non carica tutte le bandiere per problemi di connessione.
-                    this.flags();
-                    //metodo copertina
-                    this.PosterPath();
-                    //metodo ordinamento per popolarita'
-                    this.IsPopular();
-                    //metodo genere e cast
-                    this.GenreNCast();       
-                    //codice filtro
-                    //si possono selezionare piu' filtri
-                    if (this.genreselected.length > 0) {
-                        console.log(this.genreselected);
-                        this.movies.forEach((element) => {
-                            element.visible = false;
-                            for (let i = 0; i < element.genre.length; i++) {
-            
-                                if (this.genreselected.includes(element.genre[i])) {
-                                    element.visible = true;
-                                }
-            
-                            }
-            
-                        });    
-                    }
+                if (this.verify[0] == true) {
 
-                    if (this.typeselected != '') {
-
-                        this.movies.forEach((element) => {
-
-                            if ((this.typeselected == element.type) && (element.visible == true)) {
-                                element.visible = true;
-                            } else {
+                    axios
+                    .get('https://api.themoviedb.org/3/search/tv?api_key=1054834a21f5e84aed95192bd4b277cd&', { params: { query: this.search } })
+                    .then((re) => {
+                        //metodo adattamento serie TV
+                        this.convert(re);
+                        console.log(this.movies);
+                        //metodo bandiere. A volte non carica tutte le bandiere per problemi di connessione.
+                        this.flags();
+                        //metodo copertina
+                        this.PosterPath();
+                        //metodo ordinamento per popolarita'
+                        this.IsPopular();
+                        //metodo genere e cast
+                        this.GenreNCast();       
+                        //codice filtro
+                        //si possono selezionare piu' filtri
+                        if (this.genreselected.length > 0) {
+                            console.log(this.genreselected);
+                            this.movies.forEach((element) => {
                                 element.visible = false;
-                            }
-
-                        });
-
-                    }    
-                });
+                                for (let i = 0; i < element.genre.length; i++) {
+                
+                                    if (this.genreselected.includes(element.genre[i])) {
+                                        element.visible = true;
+                                    }
+                
+                                }
+                
+                            });    
+                        }
+    
+                        if (this.typeselected != '') {
+    
+                            this.movies.forEach((element) => {
+    
+                                if ((this.typeselected == element.type) && (element.visible == true)) {
+                                    element.visible = true;
+                                } else {
+                                    element.visible = false;
+                                }
+    
+                            });
+    
+                        }
+                        
+                        this.verify[2] == true;
+                    });
+    
+                }
 
             }
         },
@@ -118,6 +127,7 @@ var app = new Vue({
                 element.type ='Tv-Series'
                 element.visible = true;                
                 this.movies.push(element);
+                this.verify[1] == true;
             });
         },
         IsPopular: function() {
@@ -169,34 +179,38 @@ var app = new Vue({
                 //tentativo con AJAX in vanilla JS per velocita' risposta, solo il primo cast visualizzabile
                 //mancata visualizzazione a schermo del cast, forse per mia connessione lenta, ma visibile nella console
                 //anomalia: dopo la ricerca, premendo un tasto qualsiasi con la searchbar attiva si ha una parziale visualizzazione dei cast, ma non c'e' riaggiornamento pagina... 
-                //concatenato AJAX in SEARCHING(): e' ancora piu' lento, non funziona...
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
+                //concatenato AJAX in SEARCHING() e' piu' lento, non funziona...
+                if (this.verify[1] == true) {
 
-                        if (JSON.parse(xhttp.response).cast.length >= 5) {
-                            element.cast = [];
-
-                            for (let i = 0; i < 5; i++) {
-                                element.cast.push(JSON.parse(xhttp.response).cast[i].name);
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.onreadystatechange = function () {
+                        if (this.readyState == 4 && this.status == 200) {
+    
+                            if (JSON.parse(xhttp.response).cast.length >= 5) {
+                                element.cast = [];
+    
+                                for (let i = 0; i < 5; i++) {
+                                    element.cast.push(JSON.parse(xhttp.response).cast[i].name);
+                                }
+                            
+                            } else if ((JSON.parse(xhttp.response).cast.length == 0)) {
+                                element.cast = ['N/A'];
+                            } else {
+    
+                                for (let i = 0; i < JSON.parse(xhttp.response).cast.length; i++) {
+                                    element.cast.push(JSON.parse(xhttp.response).cast[i].name);
+                                }
+    
                             }
-                        
-                        } else if ((JSON.parse(xhttp.response).cast.length == 0)) {
-                            element.cast = ['N/A'];
-                        } else {
-
-                            for (let i = 0; i < JSON.parse(xhttp.response).cast.length; i++) {
-                                element.cast.push(JSON.parse(xhttp.response).cast[i].name);
-                            }
-
+    
                         }
-
-                    }
-                };
-                xhttp.open("GET",
-                `http://api.themoviedb.org/3/movie/${element.id}/casts?api_key=1054834a21f5e84aed95192bd4b277cd`, true);
-                xhttp.send();
-
+                    };
+                    xhttp.open("GET",
+                    `http://api.themoviedb.org/3/movie/${element.id}/casts?api_key=1054834a21f5e84aed95192bd4b277cd`, true);
+                    xhttp.send();
+    
+                }
+                
             });
 
         },
